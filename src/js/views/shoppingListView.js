9d48f330e4f0495b669f09bae24813de266d0c28
ArnaudHalvick <arnaud.halvick@gmail.com>
2024-09-25 14:23:39 -0300

    Add shopping list overlay

--- src/js/views/shoppingListView.js
+++ src/js/views/shoppingListView.js
@@ -5,6 +5,7 @@ import View from "./View.js";
 class ShoppingListView extends View {
   _parentElement = document.querySelector(".shopping-list-content");
   _window = document.querySelector(".shopping-list-window");
+  _overlay = document.querySelector(".overlay--shopping-list");
   _btnOpen = document.querySelector(".nav__btn--shopping-list");
   _btnClose = document.querySelector(".btn--close-shopping-list");
 
@@ -22,14 +23,17 @@ class ShoppingListView extends View {
   // Method to close the modal
   _addHandlerHideWindow() {
     this._btnClose.addEventListener("click", this.toggleWindow.bind(this));
+    this._overlay.addEventListener("click", this.toggleWindow.bind(this));
   }
 
   // Toggle modal visibility
   toggleWindow() {
+    this._overlay.classList.toggle("hidden");
     this._window.classList.toggle("hidden");
   }
 
   openWindow() {
+    this._overlay.classList.remove("hidden");
     this._window.classList.remove("hidden");
   }
 
